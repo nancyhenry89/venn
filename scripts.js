40dd009da75b689e0477f530d0f3bba82014c43d
nancyhenry89 <nancyhenry89@gmail.com>
2017-04-03 16:11:08 -0500

    D

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 $('document').ready(function(){
-  $.getJSON("https://github.com/nancyhenry89/venn/blob/master/data.json#L3", function(json) {
+  $.getJSON("http://github.com/nancyhenry89/venn/blob/master/data.json#L3", function(json) {
     console.log(json); // this will show the info it in firebug console
 })
 });
