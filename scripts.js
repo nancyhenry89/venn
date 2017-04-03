2ebb6dbb6d6eb4cb5a2df37cb20af189e0702619
nancyhenry89 <nancyhenry89@gmail.com>
2017-04-03 16:09:59 -0500

    File path

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 $('document').ready(function(){
-  $.getJSON("data.json", function(json) {
+  $.getJSON("https://github.com/nancyhenry89/venn/blob/master/data.json#L3", function(json) {
     console.log(json); // this will show the info it in firebug console
 })
 });
- 
